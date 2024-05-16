d80bd2f1c8fae4de3938f479dc3ecffefff35234
Ronan Garrigues <ronan.garrigues@etu.univ-amu.fr>
2024-05-16 12:15:00 +0200

    Ajout Exercice 6 début

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
-open module tp.intro.javafx {
+module tp.intro.javafx {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
+    opens fr.amu.iut.tp3 to javafx.fxml;
+    exports fr.amu.iut.tp3;
 }
